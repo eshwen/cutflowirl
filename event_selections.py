c087713d4bda1a3d0ad265c0cf78ee7de878bd5c
Tai Sakuma <tai.sakuma@gmail.com>
2015-10-24 11:15:08 +0200

    add a tigher jet id to lead jet

--- event_selections.py
+++ event_selections.py
@@ -355,6 +355,8 @@ def CommonFinalSelection(metnohf,
     ret.add(LambdaStr("ev : ev.nJet40Fwd[0] == 0", name = 'FwJetVeto'))
     ret.add(LambdaStr("ev : ev.nJet40failedId[0] == 0", name = 'JetIDVeto'))
     ret.add(LambdaStr("ev : ev.ht40[0] >= 200", name = 'HTGT200'))
+    ret.add(LambdaStr("ev : -2.5 < ev.jet_eta[0] < 2.5", name = 'LeadJetEtaLT2p5'))
+    ret.add(LambdaStr("ev : ev.jet_chHEF[0] >= 0.1", name = 'LeadJetChHEFGT0p1'))
 
     if metnohf:
         ret.add(LambdaStr("ev : ev.MhtOverMetNoXNoHF[0] < 1.25", name = 'MhtOverMetNoXNoHF'))
