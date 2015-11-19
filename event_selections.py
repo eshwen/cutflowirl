bd7f7f9d1faa73e4818276431af6d6fb247f5f7d
Tai Sakuma <tai.sakuma@gmail.com>
2015-11-19 08:32:38 +0000

    add new function AlphaTCutLoose()

--- event_selections.py
+++ event_selections.py
@@ -151,6 +151,28 @@ def PD_HLT(AllClass = EventSelectionAll, AnyClass = EventSelectionAny):
     return ret
 
 ##__________________________________________________________________||
+def AlphaTCutLoose(AllClass = EventSelectionAll, AnyClass = EventSelectionAny):
+
+    ret = AnyClass(name = 'AlphaTCutLoose')
+
+    htbin = AllClass(name = 'HT200to250')
+    htbin.add(LambdaStr("ev : 200 <= ev.ht40[0] < 250", name = 'HTin200to250'))
+    htbin.add(LambdaStr("ev : 0.60 <= ev.alphaT[0]", name = 'alphaTGT0p65'))
+    ret.add(htbin)
+
+    htbin = AllClass(name = 'HT250to300')
+    htbin.add(LambdaStr("ev : 250 <= ev.ht40[0] < 300", name = 'HTin250to300'))
+    htbin.add(LambdaStr("ev : 0.55 <= ev.alphaT[0]", name = 'alphaTGT0p60'))
+    ret.add(htbin)
+
+    htbin = AllClass(name = 'HT300to800')
+    htbin.add(LambdaStr("ev : 300 <= ev.ht40[0] < 800", name = 'HTin300to800'))
+    htbin.add(LambdaStr("ev : 0.50 <= ev.alphaT[0]", name = 'alphaTGT0p55'))
+    ret.add(htbin)
+
+    return ret
+
+##__________________________________________________________________||
 def AlphaTCut(AllClass = EventSelectionAll, AnyClass = EventSelectionAny):
 
     ret = AnyClass(name = 'AlphaTCut')
@@ -394,11 +416,11 @@ def SignalLooseSelection(datamc, pd, hlt,
 
     ## asymjet
     asymjet.add(LambdaStr("ev : ev.bintype[0] == 'asymjet'", name = 'bintype_asymjet'))
-    asymjet.add(LambdaStr("ev : 0.5 <= ev.alphaT[0]", name = 'alphaTLT0p5'))
+    asymjet.add(AlphaTCutLoose())
 
     ## symjet
     symjet.add(LambdaStr("ev : ev.bintype[0] == 'symjet'", name = 'bintype_symjet'))
-    symjet.add(LambdaStr("ev : 0.5 <= ev.alphaT[0]", name = 'alphaTLT0p5'))
+    symjet.add(AlphaTCutLoose())
 
     ## highht
     highht.add(LambdaStr("ev : ev.bintype[0] == 'highht'", name = 'bintype_highht'))
