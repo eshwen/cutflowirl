f3b9f394535790ca522352f539b7f5be6394459e
Tai Sakuma <tai.sakuma@gmail.com>
2015-11-29 15:33:08 +0100

    update test_level1.py test_level2.py

--- EventSelectionModules/test_level2.py
+++ EventSelectionModules/test_level2.py
@@ -1,8 +1,8 @@
 
 ##__________________________________________________________________||
 def test_level2(AllClass, AnyClass, **kargs):
-
     ret = AnyClass(name = 'test_level2')
+    # print 'test_level2', kargs
     return ret
 
 ##__________________________________________________________________||
