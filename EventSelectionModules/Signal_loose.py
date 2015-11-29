d858040403386b7450af3f731ecf27c038d0473e
Tai Sakuma <tai.sakuma@gmail.com>
2015-11-29 17:33:21 +0000

    fix ht40 selection in Signal_loose

--- EventSelectionModules/Signal_loose.py
+++ EventSelectionModules/Signal_loose.py
@@ -6,7 +6,7 @@ def Signal_loose(AllClass, AnyClass, datamc, **kargs):
 
     ret = AllClass(name = 'Signal_loose')
 
-    ret.add(LambdaStr("ev : ev.ht40JECDown[0] >= 200", name = 'HTGT200'))
+    ret.add(LambdaStr("ev : ev.ht40[0] >= 200", name = 'HTGT200'))
 
     bintypes = AnyClass(name = 'SignalLooseBintypes')
     ret.add(bintypes)
