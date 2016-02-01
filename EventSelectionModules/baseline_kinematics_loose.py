d43aca76ac5f90c55264a64414e550e1e82df6ce
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-01 10:17:11 +0100

    rename baseline_kinematics baseline_kinematics_loose

--- EventSelectionModules/baseline_kinematics_loose.py
+++ EventSelectionModules/baseline_kinematics_loose.py
@@ -1,8 +1,8 @@
 from .LambdaStr import LambdaStr
 
 ##__________________________________________________________________||
-def baseline_kinematics(AllClass, AnyClass, **kargs):
-    ret = AllClass(name = 'baseline_kinematics')
+def baseline_kinematics_loose(AllClass, AnyClass, **kargs):
+    ret = AllClass(name = 'baseline_kinematics_loose')
     ret.add(LambdaStr("ev : ev.nJet100[0] >= 1", name = 'nJetGTOne'))
     ret.add(LambdaStr("ev : ev.ht40[0] >= 150", name = 'HTGT150'))
     return ret
