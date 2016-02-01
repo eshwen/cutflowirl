cc1cf3b2e73ccca20ca5fd06a0525cf9634f9233
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-01 10:50:52 +0100

    update baseline_kinematics()

--- EventSelectionModules/baseline_kinematics.py
+++ EventSelectionModules/baseline_kinematics.py
@@ -2,19 +2,19 @@ from .LambdaStr import LambdaStr
 
 ##__________________________________________________________________||
 def baseline_kinematics(AllClass, AnyClass, metnohf, **kargs):
-
     ret = AllClass(name = 'baseline_kinematics')
-
-    ret.add(LambdaStr("ev : ev.nJet40Fwd[0] == 0", name = 'FwJetVeto'))
-    ret.add(LambdaStr("ev : ev.nJet40failedId[0] == 0", name = 'JetIDVeto'))
+    ret.add(LambdaStr("ev : ev.nJet100[0] >= 1", name = 'nJetGTOne'))
     ret.add(LambdaStr("ev : ev.ht40[0] >= 200", name = 'HTGT200'))
-    ret.add(LambdaStr("ev : -2.5 < ev.jet_eta[0] < 2.5", name = 'LeadJetEtaLT2p5'))
-    ret.add(LambdaStr("ev : ev.jet_chHEF[0] >= 0.1", name = 'LeadJetChHEFGT0p1'))
+    ret.add(LambdaStr("ev : ev.nJet40Fwd[0] == 0", name = 'FwJetVeto'))
 
     if metnohf:
         ret.add(LambdaStr("ev : ev.MhtOverMetNoXNoHF[0] < 1.25", name = 'MhtOverMetNoXNoHF'))
     else:
         ret.add(LambdaStr("ev : ev.MhtOverMetNoX[0] < 1.25", name = 'MhtOverMetNoX'))
+
+    ret.add(LambdaStr("ev : ev.nJet40failedId[0] == 0", name = 'JetIDVeto'))
+    ret.add(LambdaStr("ev : -2.5 < ev.jet_eta[0] < 2.5", name = 'LeadJetEtaLT2p5'))
+    ret.add(LambdaStr("ev : ev.jet_chHEF[0] >= 0.1", name = 'LeadJetChHEFGT0p1'))
 
     return ret
 ##__________________________________________________________________||
