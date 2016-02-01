7a7516f37b3efe039c6fe844b9ca79aa78fbbb3e
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-01 21:05:38 +0100

    add a new file bintype_biasedDPhi.py

---
+++ EventSelectionModules/bintype_biasedDPhi.py
@@ -0,0 +1,38 @@
+from .LambdaStr import LambdaStr
+
+##__________________________________________________________________||
+def bintype_biasedDPhi(AllClass, AnyClass, **kargs):
+
+    ret = AllClass(name = 'bintype_biasedDPhi')
+    bintypes = AnyClass(name = 'bintypes')
+    ret.add(bintypes)
+
+    monojet = AllClass(name = 'monojet')
+    asymjet = AllClass(name = 'asymjet')
+    symjet = AllClass(name = 'symjet')
+    highht = AllClass(name = 'highht')
+
+    bintypes.add(monojet)
+    bintypes.add(asymjet)
+    bintypes.add(symjet)
+    bintypes.add(highht)
+
+    ## monojet
+    monojet.add(LambdaStr("ev : ev.bintypeId[0] == 1 # 'monojet'", name = 'bintype_monojet'))
+    monojet.add(LambdaStr("ev : ev.biasedDPhi20[0] == -1 or 0.5 <= ev.biasedDPhi20[0]", name = 'biasedDPhiGT0p5'))
+
+    ## asymjet
+    asymjet.add(LambdaStr("ev : ev.bintypeId[0] == 2 # 'asymjet'", name = 'bintype_asymjet'))
+    asymjet.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
+
+    ## symjet
+    symjet.add(LambdaStr("ev : ev.bintypeId[0] == 3 # 'symjet'", name = 'bintype_symjet'))
+    symjet.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
+
+    ## highht
+    highht.add(LambdaStr("ev : ev.bintypeId[0] == 4 # 'highht'", name = 'bintype_highht'))
+    highht.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
+
+    return ret
+
+##__________________________________________________________________||
