057c4b8f0e309e1530370de5789045ad5a77149d
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-01 21:34:00 +0100

    remove biasedDPhi cut from Signal_final

--- EventSelectionModules/Signal_final.py
+++ EventSelectionModules/Signal_final.py
@@ -27,16 +27,13 @@ def Signal_final(AllClass, AnyClass, **kargs):
     ## asymjet
     asymjet.add(LambdaStr("ev : ev.bintypeId[0] == 2 # 'asymjet'", name = 'bintype_asymjet'))
     asymjet.add(AlphaTCut(AllClass, AnyClass))
-    asymjet.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
 
     ## symjet
     symjet.add(LambdaStr("ev : ev.bintypeId[0] == 3 # 'symjet'", name = 'bintype_symjet'))
     symjet.add(AlphaTCut(AllClass, AnyClass))
-    symjet.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
 
     ## highht
     highht.add(LambdaStr("ev : ev.bintypeId[0] == 4 # 'highht'", name = 'bintype_highht'))
-    highht.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
 
     return ret
 
