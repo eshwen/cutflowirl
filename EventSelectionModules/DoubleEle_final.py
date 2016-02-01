037432d98f94cc010ae196cc6249cd12fc353c5a
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-01 22:28:49 +0100

    use nMuonsIsolated, nElectronsIsolated for event selections

--- EventSelectionModules/DoubleEle_final.py
+++ EventSelectionModules/DoubleEle_final.py
@@ -7,8 +7,7 @@ def DoubleEle_final(AllClass, AnyClass, datamc, **kargs):
 
     ret.add(LambdaStr("ev : -1.479 < ev.ele_eta[0] < 1.479", name = 'eleBarrel'))
     ret.add(LambdaStr("ev : -1.479 < ev.ele_eta[1] < 1.479", name = 'eleBarrel'))
-    ret.add(LambdaStr("ev : ev.ele_relIso04[0] < 0.0354", name = 'eleRelIso04'))
-    ret.add(LambdaStr("ev : ev.ele_relIso04[1] < 0.0354", name = 'eleRelIso04'))
+    ret.add(LambdaStr("ev : ev.nElectronsIsolated[0] == 2", name = 'nElectronsIsolated'))
     ret.add(LambdaStr("ev : ev.nIsoTracksNoEleVeto[0] <= 0", name = 'isoTrackNoEleVeto'))
     ret.add(LambdaStr("ev : 66.2 <= ev.mll[0] < 116.2", name = 'mll'))
     ret.add(LambdaStr("ev : ev.minDelRJetEle[0] >= 0.5", name = 'minDelRJetEle'))
