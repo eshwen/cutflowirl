80aff8c168e20e854db185b48455a837e14f73af
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-08 13:04:37 +0100

    replace test_event_selection_str with event_selection_str in Test_cutflows

--- tests/EventSelectionModules/test_cutflows.py
+++ tests/EventSelectionModules/test_cutflows.py
@@ -2,6 +2,7 @@ from ...EventSelectionModules.cutflows import cutflows
 from ...EventSelectionModules.EventSelectionAll import EventSelectionAll
 from ...EventSelectionModules.EventSelectionAny import EventSelectionAny
 from ...EventSelectionModules.LambdaStr import LambdaStr
+from ...event_selection_str import event_selection_str
 import unittest
 
 ##__________________________________________________________________||
@@ -25,59 +26,6 @@ class Test_cutflows(unittest.TestCase):
             ))
 
         es = cutflows(EventSelectionAll, EventSelectionAny, **kargs)
-        ## print test_event_selection_str(es)
-
-##__________________________________________________________________||
-def test_event_selection_str(eventSelection):
-    out = test_event_selection_io(eventSelection)
-    return out.getvalue()
-
-##__________________________________________________________________||
-def test_event_selection_io(eventSelection, out = None, prep = ''):
-
-    if out is None:
-        import StringIO
-        out = StringIO.StringIO()
-
-    import inspect
-
-    def print_name(es):
-        ret = '<'
-        if hasattr(es, 'name'):
-            ret += str(es.name)
-        ret += (':')
-        if inspect.isfunction(es):
-            ret += es.__name__
-        else:
-            ret += es.__class__.__name__
-        ret += '>'
-        return ret
-
-    out.write(prep)
-
-    if isinstance(eventSelection, LambdaStr):
-        out.write(print_name(eventSelection))
-        out.write(' ')
-        out.write(eventSelection.lambda_str)
-        out.write('\n')
-        return out
-
-    if isinstance(eventSelection, EventSelectionAll):
-        out.write(print_name(eventSelection))
-        out.write('\n')
-        for e in eventSelection.selections:
-            test_event_selection_io(e, out, prep + '  ')
-        return out
-
-    if isinstance(eventSelection, EventSelectionAny):
-        out.write(print_name(eventSelection))
-        out.write('\n')
-        for e in eventSelection.selections:
-            test_event_selection_io(e, out, prep + '  ')
-        return out
-
-    out.write(print_name(eventSelection))
-    out.write('\n')
-    return out
+        ## print event_selection_str(es)
 
 ##__________________________________________________________________||
