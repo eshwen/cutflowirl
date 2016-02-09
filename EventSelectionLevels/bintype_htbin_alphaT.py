25c9152132973da3daff5a5156f7ba2fec314107
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-09 18:05:02 +0100

    add a new file bintype_htbin_alphaT.py

---
+++ EventSelectionLevels/bintype_htbin_alphaT.py
@@ -0,0 +1,37 @@
+from .Modules.LambdaStr import LambdaStr
+from .htbin_alphaT import htbin_alphaT
+
+##__________________________________________________________________||
+def bintype_htbin_alphaT(AllClass, AnyClass, **kargs):
+
+    ret = AllClass(name = 'bintype_htbin_alphaT')
+    bintypes = AnyClass(name = 'bintypes')
+    ret.add(bintypes)
+
+    monojet = AllClass(name = 'monojet')
+    asymjet = AllClass(name = 'asymjet')
+    symjet = AllClass(name = 'symjet')
+    highht = AllClass(name = 'highht')
+
+    bintypes.add(monojet)
+    bintypes.add(asymjet)
+    bintypes.add(symjet)
+    bintypes.add(highht)
+
+    ## monojet
+    monojet.add(LambdaStr("ev : ev.bintypeId[0] == 1 # 'monojet'", name = 'bintype_monojet'))
+
+    ## asymjet
+    asymjet.add(LambdaStr("ev : ev.bintypeId[0] == 2 # 'asymjet'", name = 'bintype_asymjet'))
+    asymjet.add(htbin_alphaT(AllClass, AnyClass))
+
+    ## symjet
+    symjet.add(LambdaStr("ev : ev.bintypeId[0] == 3 # 'symjet'", name = 'bintype_symjet'))
+    symjet.add(htbin_alphaT(AllClass, AnyClass))
+
+    ## highht
+    highht.add(LambdaStr("ev : ev.bintypeId[0] == 4 # 'highht'", name = 'bintype_highht'))
+
+    return ret
+
+##__________________________________________________________________||
