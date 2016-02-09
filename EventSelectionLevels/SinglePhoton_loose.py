d68c14a7e2b1e45837ece1abd85457ea751276d6
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-09 20:57:23 +0100

    remove the mht cut from Signal_loose, Signal_loose_JECvariation, SinglePhoton_loose, SinglePhoton_loose_JECvariation

--- EventSelectionLevels/SinglePhoton_loose.py
+++ EventSelectionLevels/SinglePhoton_loose.py
@@ -2,35 +2,7 @@ from .Modules.LambdaStr import LambdaStr
 
 ##__________________________________________________________________||
 def SinglePhoton_loose(AllClass, AnyClass, datamc, **kargs):
-
     ret = AllClass(name = 'SinglePhoton_loose')
-
-    bintypes = AnyClass(name = 'SinglePhotonLooseBintypes')
-    ret.add(bintypes)
-
-    monojet = AllClass(name = 'SinglePhotonLooseMonojet')
-    asymjet = AllClass(name = 'SinglePhotonLooseAsymjet')
-    symjet = AllClass(name = 'SinglePhotonLooseSymjet')
-    highht = AllClass(name = 'SinglePhotonLooseHighht')
-
-    bintypes.add(monojet)
-    bintypes.add(asymjet)
-    bintypes.add(symjet)
-    bintypes.add(highht)
-
-    ## monojet
-    monojet.add(LambdaStr("ev : ev.bintypeId[0] == 1 # 'monojet'", name = 'bintype_monojet'))
-
-    ## asymjet
-    asymjet.add(LambdaStr("ev : ev.bintypeId[0] == 2 # 'asymjet'", name = 'bintype_asymjet'))
-
-    ## symjet
-    symjet.add(LambdaStr("ev : ev.bintypeId[0] == 3 # 'symjet'", name = 'bintype_symjet'))
-
-    ## highht
-    highht.add(LambdaStr("ev : ev.bintypeId[0] == 4 # 'highht'", name = 'bintype_highht'))
-    highht.add(LambdaStr("ev : 130 <= ev.mht40_pt[0]", name = 'MHTGT130'))
-
     return ret
 
 ##__________________________________________________________________||
--- EventSelectionLevels/SinglePhoton_loose_JECvariation.py
+++ EventSelectionLevels/SinglePhoton_loose_JECvariation.py
@@ -2,35 +2,7 @@ from .Modules.LambdaStr import LambdaStr
 
 ##__________________________________________________________________||
 def SinglePhoton_loose_JECvariation(AllClass, AnyClass, datamc, **kargs):
-
     ret = AllClass(name = 'SinglePhoton_loose_JECvariation')
-
-    bintypes = AnyClass(name = 'SinglePhotonLooseBintypes')
-    ret.add(bintypes)
-
-    monojet = AllClass(name = 'SinglePhotonLooseMonojet')
-    asymjet = AllClass(name = 'SinglePhotonLooseAsymjet')
-    symjet = AllClass(name = 'SinglePhotonLooseSymjet')
-    highht = AllClass(name = 'SinglePhotonLooseHighht')
-
-    bintypes.add(monojet)
-    bintypes.add(asymjet)
-    bintypes.add(symjet)
-    bintypes.add(highht)
-
-    ## monojet
-    monojet.add(LambdaStr("ev : 1 in (ev.bintypeId[0], ev.bintypeIdJECUp[0], ev.bintypeIdJECDown[0]) # 'monojet'", name = 'bintype_monojet'))
-
-    ## asymjet
-    asymjet.add(LambdaStr("ev : 2 in (ev.bintypeId[0], ev.bintypeIdJECUp[0], ev.bintypeIdJECDown[0]) # 'asymjet'", name = 'bintype_asymjet'))
-
-    ## symjet
-    symjet.add(LambdaStr("ev : 3 in (ev.bintypeId[0], ev.bintypeIdJECUp[0], ev.bintypeIdJECDown[0]) # 'symjet'", name = 'bintype_symjet'))
-
-    ## highht
-    highht.add(LambdaStr("ev : 4 in (ev.bintypeId[0], ev.bintypeIdJECUp[0], ev.bintypeIdJECDown[0]) # 'highht'", name = 'bintype_highht'))
-    highht.add(LambdaStr("ev : 130 <= max(ev.mht40_pt[0], ev.mht40JECUp_pt[0], ev.mht40JECDown_pt[0])", name = 'MHTGT130'))
-
     return ret
 
 ##__________________________________________________________________||
