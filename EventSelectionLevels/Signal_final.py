37d1985fb3726a99b38c1d30993f9f459d617b3e
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-09 18:15:05 +0100

    clean up Signal_final, SinglePhoton_final

--- EventSelectionLevels/Signal_final.py
+++ EventSelectionLevels/Signal_final.py
@@ -1,40 +1,9 @@
 from .Modules.LambdaStr import LambdaStr
-from .htbin_alphaT import htbin_alphaT
 
 ##__________________________________________________________________||
 def Signal_final(AllClass, AnyClass, **kargs):
-
     ret = AllClass(name = 'Signal_final')
-
     ret.add(LambdaStr("ev : ev.nIsoTracksVeto[0] <= 0", name = 'isoTrackVeto'))
-
-    bintypes = AnyClass(name = 'SignalBintypes')
-    ret.add(bintypes)
-
-    monojet = AllClass(name = 'SignalFinalMonojet')
-    asymjet = AllClass(name = 'SignalFinalAsymjet')
-    symjet = AllClass(name = 'SignalFinalSymjet')
-    highht = AllClass(name = 'SignalFinalHighht')
-
-    bintypes.add(monojet)
-    bintypes.add(asymjet)
-    bintypes.add(symjet)
-    bintypes.add(highht)
-
-    ## monojet
-    monojet.add(LambdaStr("ev : ev.bintypeId[0] == 1 # 'monojet'", name = 'bintype_monojet'))
-
-    ## asymjet
-    asymjet.add(LambdaStr("ev : ev.bintypeId[0] == 2 # 'asymjet'", name = 'bintype_asymjet'))
-    asymjet.add(htbin_alphaT(AllClass, AnyClass))
-
-    ## symjet
-    symjet.add(LambdaStr("ev : ev.bintypeId[0] == 3 # 'symjet'", name = 'bintype_symjet'))
-    symjet.add(htbin_alphaT(AllClass, AnyClass))
-
-    ## highht
-    highht.add(LambdaStr("ev : ev.bintypeId[0] == 4 # 'highht'", name = 'bintype_highht'))
-
     return ret
 
 ##__________________________________________________________________||
