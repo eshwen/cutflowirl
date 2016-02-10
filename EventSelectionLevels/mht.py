49520e2bccd70367724dd521b8bf4ccf2777eded
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-10 16:20:36 +0100

    add a new file mht.py

---
+++ EventSelectionLevels/mht.py
@@ -0,0 +1,7 @@
+from .Modules.LambdaStr import LambdaStr
+
+##__________________________________________________________________||
+def mht(AllClass, AnyClass, **kargs):
+    return LambdaStr("ev : 130 <= ev.mht40_pt[0]", name = 'mht')
+
+##__________________________________________________________________||
