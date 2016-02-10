342ccb11d97d0b92651c0e9232dde8088d1f18b0
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-10 15:00:26 +0100

    use new files in bintype_biasedDPhi

--- EventSelectionLevels/bintype_biasedDPhi.py
+++ EventSelectionLevels/bintype_biasedDPhi.py
@@ -1,4 +1,10 @@
 from .Modules.LambdaStr import LambdaStr
+from .bintype_monojet import bintype_monojet
+from .bintype_asymjet import bintype_asymjet
+from .bintype_symjet import bintype_symjet
+from .bintype_highht import bintype_highht
+from .biasedDPhi import biasedDPhi
+from .biasedDPhi20 import biasedDPhi20
 
 ##__________________________________________________________________||
 def bintype_biasedDPhi(AllClass, AnyClass, **kargs):
@@ -18,20 +24,20 @@ def bintype_biasedDPhi(AllClass, AnyClass, **kargs):
     bintypes.add(highht)
 
     ## monojet
-    monojet.add(LambdaStr("ev : ev.bintypeId[0] == 1 # 'monojet'", name = 'bintype_monojet'))
-    monojet.add(LambdaStr("ev : ev.biasedDPhi20[0] == -1 or 0.5 <= ev.biasedDPhi20[0]", name = 'biasedDPhiGT0p5'))
+    monojet.add(bintype_monojet(AllClass, AnyClass, **kargs))
+    monojet.add(biasedDPhi20(AllClass, AnyClass, **kargs))
 
     ## asymjet
-    asymjet.add(LambdaStr("ev : ev.bintypeId[0] == 2 # 'asymjet'", name = 'bintype_asymjet'))
-    asymjet.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
+    asymjet.add(bintype_asymjet(AllClass, AnyClass, **kargs))
+    asymjet.add(biasedDPhi(AllClass, AnyClass, **kargs))
 
     ## symjet
-    symjet.add(LambdaStr("ev : ev.bintypeId[0] == 3 # 'symjet'", name = 'bintype_symjet'))
-    symjet.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
+    symjet.add(bintype_symjet(AllClass, AnyClass, **kargs))
+    symjet.add(biasedDPhi(AllClass, AnyClass, **kargs))
 
     ## highht
-    highht.add(LambdaStr("ev : ev.bintypeId[0] == 4 # 'highht'", name = 'bintype_highht'))
-    highht.add(LambdaStr("ev : 0.5 <= ev.biasedDPhi[0]", name = 'biasedDPhiGT0p5'))
+    highht.add(bintype_highht(AllClass, AnyClass, **kargs))
+    highht.add(biasedDPhi(AllClass, AnyClass, **kargs))
 
     return ret
 
