fe37802e6639349ee66c16507d4cf603d837d3c4
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-10 16:28:18 +0100

    add kargs to the arguments of htbin_alphaT

--- EventSelectionLevels/htbin_alphaT.py
+++ EventSelectionLevels/htbin_alphaT.py
@@ -1,7 +1,7 @@
 from .Modules.LambdaStr import LambdaStr
 
 ##__________________________________________________________________||
-def htbin_alphaT(AllClass, AnyClass):
+def htbin_alphaT(AllClass, AnyClass, **kargs):
 
     ret = AnyClass(name = 'htbin_alphaT')
 
