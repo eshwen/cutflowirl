a95c400cd12023a14b680a212859d8e021cff2b0
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-10 16:34:43 +0100

    use AlternativeSequences in bintype_htbin_alphaT

--- EventSelectionLevels/bintype_htbin_alphaT.py
+++ EventSelectionLevels/bintype_htbin_alphaT.py
@@ -1,37 +1,18 @@
 from .Modules.LambdaStr import LambdaStr
-from .htbin_alphaT import htbin_alphaT
+from .AlternativeSequences import AlternativeSequences
 
 ##__________________________________________________________________||
 def bintype_htbin_alphaT(AllClass, AnyClass, **kargs):
+    kargs = kargs.copy()
+    kargs.update(dict(
+        name = 'bintype_htbin_alphaT',
+        sequences = (
+            dict(name = 'monojet', levels =('bintype_monojet', )),
+            dict(name = 'asymjet', levels =('bintype_asymjet', 'htbin_alphaT')),
+            dict(name = 'symjet', levels =('bintype_symjet', 'htbin_alphaT')),
+            dict(name = 'highht', levels =('bintype_highht', )),
+        )))
 
-    ret = AllClass(name = 'bintype_htbin_alphaT')
-    bintypes = AnyClass(name = 'bintypes')
-    ret.add(bintypes)
-
-    monojet = AllClass(name = 'monojet')
-    asymjet = AllClass(name = 'asymjet')
-    symjet = AllClass(name = 'symjet')
-    highht = AllClass(name = 'highht')
-
-    bintypes.add(monojet)
-    bintypes.add(asymjet)
-    bintypes.add(symjet)
-    bintypes.add(highht)
-
-    ## monojet
-    monojet.add(LambdaStr("ev : ev.bintypeId[0] == 1 # 'monojet'", name = 'bintype_monojet'))
-
-    ## asymjet
-    asymjet.add(LambdaStr("ev : ev.bintypeId[0] == 2 # 'asymjet'", name = 'bintype_asymjet'))
-    asymjet.add(htbin_alphaT(AllClass, AnyClass))
-
-    ## symjet
-    symjet.add(LambdaStr("ev : ev.bintypeId[0] == 3 # 'symjet'", name = 'bintype_symjet'))
-    symjet.add(htbin_alphaT(AllClass, AnyClass))
-
-    ## highht
-    highht.add(LambdaStr("ev : ev.bintypeId[0] == 4 # 'highht'", name = 'bintype_highht'))
-
-    return ret
+    return AlternativeSequences(AllClass, AnyClass, **kargs)
 
 ##__________________________________________________________________||
