25d68534d256750f3637b1b88ee5c144e52378a4
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-10 16:21:43 +0100

    use AlternativeSequences in bintype_mht

--- EventSelectionLevels/bintype_mht.py
+++ EventSelectionLevels/bintype_mht.py
@@ -1,35 +1,18 @@
 from .Modules.LambdaStr import LambdaStr
+from .AlternativeSequences import AlternativeSequences
 
 ##__________________________________________________________________||
 def bintype_mht(AllClass, AnyClass, **kargs):
+    kargs = kargs.copy()
+    kargs.update(dict(
+        name = 'bintype_mht',
+        sequences = (
+            dict(name = 'monojet', levels =('bintype_monojet', )),
+            dict(name = 'asymjet', levels =('bintype_asymjet', )),
+            dict(name = 'symjet', levels =('bintype_symjet', )),
+            dict(name = 'highht', levels =('bintype_highht', 'mht')),
+        )))
 
-    ret = AllClass(name = 'bintype_mht')
-    bintypes = AnyClass(name = 'bintypes')
-    ret.add(bintypes)
-
-    monojet = AllClass(name = 'monojet')
-    asymjet = AllClass(name = 'asymjet')
-    symjet = AllClass(name = 'symjet')
-    highht = AllClass(name = 'highht')
-
-    bintypes.add(monojet)
-    bintypes.add(asymjet)
-    bintypes.add(symjet)
-    bintypes.add(highht)
-
-    ## monojet
-    monojet.add(LambdaStr("ev : ev.bintypeId[0] == 1 # 'monojet'", name = 'bintype_monojet'))
-
-    ## asymjet
-    asymjet.add(LambdaStr("ev : ev.bintypeId[0] == 2 # 'asymjet'", name = 'bintype_asymjet'))
-
-    ## symjet
-    symjet.add(LambdaStr("ev : ev.bintypeId[0] == 3 # 'symjet'", name = 'bintype_symjet'))
-
-    ## highht
-    highht.add(LambdaStr("ev : ev.bintypeId[0] == 4 # 'highht'", name = 'bintype_highht'))
-    highht.add(LambdaStr("ev : 130 <= ev.mht40_pt[0]", name = 'MHTGT130'))
-
-    return ret
+    return AlternativeSequences(AllClass, AnyClass, **kargs)
 
 ##__________________________________________________________________||
