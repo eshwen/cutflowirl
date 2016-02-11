0bb4199d7580a4f7ba36a6168cb953038dd446ab
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-11 15:14:23 +0100

    remove SinglePhoton_loose.py

--- EventSelectionLevels/SinglePhoton_loose.py
+++
@@ -1,8 +0,0 @@
-from .Modules.LambdaStr import LambdaStr
-
-##__________________________________________________________________||
-def SinglePhoton_loose(AllClass, AnyClass, datamc, **kargs):
-    ret = AllClass(name = 'SinglePhoton_loose')
-    return ret
-
-##__________________________________________________________________||
