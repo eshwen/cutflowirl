dbbbe6efd21165dfe4c3abe85784c3d8c4a56e2e
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-11 15:05:25 +0100

    add option hlt to SingleMu_HLT

--- EventSelectionLevels/SingleMu_HLT.py
+++ EventSelectionLevels/SingleMu_HLT.py
@@ -1,7 +1,8 @@
 from .Modules.LambdaStr import LambdaStr
 
 ##__________________________________________________________________||
-def SingleMu_HLT(AllClass, AnyClass, **kargs):
+def SingleMu_HLT(AllClass, AnyClass, hlt, **kargs):
+    if not hlt: return AlwaysTrue()
     ret = AnyClass(name = 'SingleMu_HLT')
     ret.add(LambdaStr("ev : ev.HLT_IsoMu17_eta2p1[0]", name = 'HLT_IsoMu17_eta2p1'))
     ret.add(LambdaStr("ev : ev.HLT_IsoMu20[0]", name = 'HLT_IsoMu20'))
