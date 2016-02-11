43a60cb31c719329a5e39aeb2bbb134a5b6fd98e
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-11 14:44:28 +0100

    add a new file mll.py

---
+++ EventSelectionLevels/mll.py
@@ -0,0 +1,7 @@
+from .Modules.LambdaStr import LambdaStr
+
+##__________________________________________________________________||
+def mll(AllClass, AnyClass, **kargs):
+    return LambdaStr("ev : 66.2 <= ev.mll[0] < 116.2", name = 'mll')
+
+##__________________________________________________________________||
