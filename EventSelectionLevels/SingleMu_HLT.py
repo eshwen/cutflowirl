19f1b7b55654bbf201fe29e081b95aee10ed16d8
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-11 13:36:17 +0100

    add HLT_IsoTkMuMu20 in SingleMu_HLT

--- EventSelectionLevels/SingleMu_HLT.py
+++ EventSelectionLevels/SingleMu_HLT.py
@@ -5,6 +5,7 @@ def SingleMu_HLT(AllClass, AnyClass, **kargs):
     ret = AnyClass(name = 'SingleMu_HLT')
     ret.add(LambdaStr("ev : ev.HLT_IsoMu17_eta2p1[0]", name = 'HLT_IsoMu17_eta2p1'))
     ret.add(LambdaStr("ev : ev.HLT_IsoMu20[0]", name = 'HLT_IsoMu20'))
+    ret.add(LambdaStr("ev : ev.HLT_IsoTkMuMu20[0]", name = 'HLT_IsoTkMuMu20'))
     ret.add(LambdaStr("ev : ev.HLT_IsoMu24_eta2p1[0]", name = 'HLT_IsoMu24_eta2p1'))
     return ret
 
