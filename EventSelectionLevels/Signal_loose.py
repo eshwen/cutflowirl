29b0fea1675aef43696f87c831cfbddb011e7437
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-11 13:35:32 +0100

    remove Signal_loose, rename Signal_final isoTrackVeto

--- EventSelectionLevels/Signal_loose.py
+++
@@ -1,9 +0,0 @@
-from .Modules.LambdaStr import LambdaStr
-
-##__________________________________________________________________||
-def Signal_loose(AllClass, AnyClass, datamc, **kargs):
-    ret = AllClass(name = 'Signal_loose')
-    ret.add(LambdaStr("ev : ev.ht40[0] >= 200", name = 'HTGT200'))
-    return ret
-
-##__________________________________________________________________||
--- EventSelectionLevels/isoTrackVeto.py
+++ EventSelectionLevels/isoTrackVeto.py
@@ -1,9 +1,7 @@
 from .Modules.LambdaStr import LambdaStr
 
 ##__________________________________________________________________||
-def Signal_final(AllClass, AnyClass, **kargs):
-    ret = AllClass(name = 'Signal_final')
-    ret.add(LambdaStr("ev : ev.nIsoTracksVeto[0] <= 0", name = 'isoTrackVeto'))
-    return ret
+def isoTrackVeto(AllClass, AnyClass, **kargs):
+    return LambdaStr("ev : ev.nIsoTracksVeto[0] <= 0", name = 'isoTrackVeto')
 
 ##__________________________________________________________________||
