11a35370283cb827fba132bf66424cc814ff856f
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-11 15:20:55 +0100

    remove SinglePhoton_loose_JECvariation.py

--- EventSelectionLevels/SinglePhoton_loose_JECvariation.py
+++
@@ -1,8 +0,0 @@
-from .Modules.LambdaStr import LambdaStr
-
-##__________________________________________________________________||
-def SinglePhoton_loose_JECvariation(AllClass, AnyClass, datamc, **kargs):
-    ret = AllClass(name = 'SinglePhoton_loose_JECvariation')
-    return ret
-
-##__________________________________________________________________||
