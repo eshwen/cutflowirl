9913910f827cf34197a53473ee1c43acc6c90e2f
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-12 10:09:52 +0100

    clean up the code in buildEventSelection

--- buildEventSelection.py
+++ buildEventSelection.py
@@ -17,11 +17,6 @@ def buildEventSelection(levels, name = 'All', AllClass = EventSelectionAll, AnyC
     eventSelection = AllClass(name = name)
 
     ##______________________________________________________________||
-    top_module_name = 'EventSelectionLevels'
-    f, filename, description = imp.find_module(top_module_name)
-    top_module = imp.load_module(top_module_name, f, filename, description)
-
-    ##______________________________________________________________||
     for level in levels:
         if isinstance(level, basestring):
             level_name, level_kargs_0 = level, { }
@@ -35,12 +30,7 @@ def buildEventSelection(levels, name = 'All', AllClass = EventSelectionAll, AnyC
         level_kargs.update(level_kargs_0)
         # e.g., level_args = {'arg1': 1, 'arg2': 2, 'datamc': 'data'}
 
-        module_name = "{}.{}".format(top_module_name, level_name)
-        # e.g., 'EventSelectionLevels.baseline_kinematics'
-
-        f, filename, description = imp.find_module(level_name, top_module.__path__)
-
-        module = imp.load_module(module_name, f, filename, description)
+        module = find_module(level_name)
 
         func = getattr(module, level_name)
 
@@ -50,5 +40,21 @@ def buildEventSelection(levels, name = 'All', AllClass = EventSelectionAll, AnyC
     
     ##______________________________________________________________||
     return eventSelection
+
+##__________________________________________________________________||
+def find_module(name):
+
+    top_module_name = 'EventSelectionLevels'
+    f, filename, description = imp.find_module(top_module_name)
+    top_module = imp.load_module(top_module_name, f, filename, description)
+    ##______________________________________________________________||
+
+    module_name = "{}.{}".format(top_module_name, name)
+    # e.g., 'EventSelectionLevels.baseline_kinematics'
+
+    f, filename, description = imp.find_module(name, top_module.__path__)
+    module = imp.load_module(module_name, f, filename, description)
+
+    return module
 
 ##__________________________________________________________________||
