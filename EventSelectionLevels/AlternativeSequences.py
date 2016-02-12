6baf268a05977047635b39ba6cd1786838805c2a
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-12 08:46:47 +0100

    clean up the code in AlternativeSequences

--- EventSelectionLevels/AlternativeSequences.py
+++ EventSelectionLevels/AlternativeSequences.py
@@ -1,25 +1,15 @@
 import imp
 
 ##__________________________________________________________________||
-def AlternativeSequences(AllClass, AnyClass, name = 'AlternativeSequences', **kargs):
+def AlternativeSequences(AllClass, AnyClass, sequences, name = 'AlternativeSequences', **kargs):
 
     ret = AnyClass(name = name)
 
     ##______________________________________________________________||
-    top_module_name = 'EventSelectionLevels'
-    f, filename, description = imp.find_module(top_module_name)
-    top_module = imp.load_module(top_module_name, f, filename, description)
-
-    ##______________________________________________________________||
-    kargs_for_sequence = kargs.copy()
-    del kargs_for_sequence['sequences']
-
-    ##______________________________________________________________||
-    sequences = kargs['sequences']
     for sequence in sequences:
 
         sequence_args_0 = sequence['kargs'] if 'kargs' in sequence else { }
-        sequence_args = kargs_for_sequence.copy()
+        sequence_args = kargs.copy()
         sequence_args.update(sequence_args_0)
 
         sequence_name = sequence['name'] if 'name' in sequence else None
@@ -36,16 +26,35 @@ def AlternativeSequences(AllClass, AnyClass, name = 'AlternativeSequences', **ka
             else:
                 level_name, level_kargs_0 = level
 
+            # e.g., level_name = 'baseline_kinematics'
+            #       level_kargs_0 = {'arg1': 1, 'arg2': 2}
+
             level_args = sequence_args.copy()
             level_args.update(level_kargs_0)
+            # e.g., level_args = {'arg1': 1, 'arg2': 2, 'datamc': 'data'}
 
-            module_name = "{}.{}".format(top_module_name, level_name)
-            f, filename, description = imp.find_module(level_name, top_module.__path__)
-            module = imp.load_module(module_name, f, filename, description)
+            module = find_module(level_name)
+
             func = getattr(module, level_name)
             selection_level = func(AllClass, AnyClass, **level_args)
             selection.add(selection_level)
 
     return ret
+
+##__________________________________________________________________||
+def find_module(name):
+
+    top_module_name = 'EventSelectionLevels'
+    f, filename, description = imp.find_module(top_module_name)
+    top_module = imp.load_module(top_module_name, f, filename, description)
+    ##______________________________________________________________||
+
+    module_name = "{}.{}".format(top_module_name, name)
+    # e.g., 'EventSelectionLevels.baseline_kinematics'
+
+    f, filename, description = imp.find_module(name, top_module.__path__)
+    module = imp.load_module(module_name, f, filename, description)
+
+    return module
 
 ##__________________________________________________________________||
