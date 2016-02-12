e23515974ed850c80792c3cf252f61b5a9f797e6
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-12 13:55:45 +0100

    move the implementation of Test_buildEventSelection to Test_AllFactory

---
+++ tests/EventSelectionLevels/test_AllFactory.py
@@ -0,0 +1,37 @@
+from ...EventSelectionLevels.AllFactory import AllFactory
+from ...EventSelectionLevels.Modules.EventSelectionAll import EventSelectionAll
+from ...EventSelectionLevels.Modules.EventSelectionAny import EventSelectionAny
+from ...EventSelectionLevels.Modules.LambdaStr import LambdaStr
+import unittest
+
+##__________________________________________________________________||
+class Test_AllFactory(unittest.TestCase):
+
+    def setUp(self):
+
+        kargs = dict(
+            arg1 = 10,
+            arg2 = 20,
+            levels = (
+                ('test_level1', dict(arg2 = 2, arg3 = 3)),
+                'test_level2',
+            ))
+
+        self.obj = AllFactory(AllClass = EventSelectionAll, AnyClass = EventSelectionAny, **kargs)
+
+    def test_obj(self):
+        self.assertIsInstance(self.obj, EventSelectionAll)
+        self.assertEqual('All', self.obj.name)
+        self.assertEqual(2, len(self.obj.selections))
+
+    def test_level1(self):
+        level = self.obj.selections[0]
+        self.assertEqual('test_level1', level.name)
+        self.assertEqual({'arg1': 10, 'arg2': 2, 'arg3': 3}, level.kargs)
+
+    def test_level2(self):
+        level = self.obj.selections[1]
+        self.assertEqual('test_level2', level.name)
+        self.assertEqual({'arg1': 10, 'arg2': 20}, level.kargs)
+
+##__________________________________________________________________||
--- tests/test_buildEventSelection.py
+++ tests/test_buildEventSelection.py
@@ -1,11 +1,10 @@
 from ..buildEventSelection import buildEventSelection
-from ..EventSelectionLevels.Modules.EventSelectionAll import EventSelectionAll
 import unittest
 
 ##__________________________________________________________________||
 class Test_buildEventSelection(unittest.TestCase):
 
-    def setUp(self):
+    def test_call(self):
 
         kargs = dict(
             arg1 = 10,
@@ -15,21 +14,6 @@ class Test_buildEventSelection(unittest.TestCase):
                 'test_level2',
             ))
 
-        self.obj = buildEventSelection(**kargs)
-
-    def test_obj(self):
-        self.assertIsInstance(self.obj, EventSelectionAll)
-        self.assertEqual('All', self.obj.name)
-        self.assertEqual(2, len(self.obj.selections))
-
-    def test_level1(self):
-        level = self.obj.selections[0]
-        self.assertEqual('test_level1', level.name)
-        self.assertEqual({'arg1': 10, 'arg2': 2, 'arg3': 3}, level.kargs)
-
-    def test_level2(self):
-        level = self.obj.selections[1]
-        self.assertEqual('test_level2', level.name)
-        self.assertEqual({'arg1': 10, 'arg2': 20}, level.kargs)
+        obj = buildEventSelection(**kargs)
 
 ##__________________________________________________________________||
