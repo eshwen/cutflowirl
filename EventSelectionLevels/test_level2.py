3d5af0e82402f26c50f1bb43ebb8a7a68a7685ad
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-14 13:03:10 +0100

    update the arguments to test_level1, test_level2

--- EventSelectionLevels/test_level2.py
+++ EventSelectionLevels/test_level2.py
@@ -1,7 +1,7 @@
 from .Modules.TestTrue import TestTrue
 
 ##__________________________________________________________________||
-def test_level2(AllClass, AnyClass, **kargs):
+def test_level2(AllClass = None, AnyClass = None, **kargs):
     ret = TestTrue(name = 'test_level2', **kargs)
     return ret
 
