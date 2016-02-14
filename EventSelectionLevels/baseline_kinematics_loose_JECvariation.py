c2df555da3e76fde989624c384809bd5dbe99556
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-14 14:15:46 +0100

    rename in baseline_kinematics_loose_JECvariation()

--- EventSelectionLevels/baseline_kinematics_loose_JECvariation.py
+++ EventSelectionLevels/baseline_kinematics_loose_JECvariation.py
@@ -3,8 +3,8 @@ from .Modules.LambdaStr import LambdaStr
 ##__________________________________________________________________||
 def baseline_kinematics_loose_JECvariation(AllClass, AnyClass, **kargs):
     ret = AllClass(name = 'baseline_kinematics_loose_JECvariation')
-    ret.add(LambdaStr("ev : ev.nJet100JECUp[0] >= 1", name = 'nJetGTOne'))
-    ret.add(LambdaStr("ev : ev.ht40JECUp[0] >= 150", name = 'HTGT150'))
+    ret.add(LambdaStr("ev : ev.nJet100JECUp[0] >= 1", name = 'nJet100_JECvar'))
+    ret.add(LambdaStr("ev : ev.ht40JECUp[0] >= 150", name = 'ht40_loose_JECvar'))
     return ret
 
 ##__________________________________________________________________||
