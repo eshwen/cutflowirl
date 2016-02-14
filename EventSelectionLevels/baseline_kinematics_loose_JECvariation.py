e17fcedab4cddcbeb85eea0c31cf46229a19656f
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-14 14:31:09 +0100

    rewrite baseline_kinematics_loose_JECvariation() with FactoryDispatcher

--- EventSelectionLevels/baseline_kinematics_loose_JECvariation.py
+++ EventSelectionLevels/baseline_kinematics_loose_JECvariation.py
@@ -1,10 +1,12 @@
-from .Modules.LambdaStr import LambdaStr
+from .FactoryDispatcher import FactoryDispatcher
 
 ##__________________________________________________________________||
-def baseline_kinematics_loose_JECvariation(AllClass, AnyClass, **kargs):
-    ret = AllClass(name = 'baseline_kinematics_loose_JECvariation')
-    ret.add(LambdaStr("ev : ev.nJet100JECUp[0] >= 1", name = 'nJet100_JECvar'))
-    ret.add(LambdaStr("ev : ev.ht40JECUp[0] >= 150", name = 'ht40_loose_JECvar'))
-    return ret
+def baseline_kinematics_loose_JECvariation(**kargs):
+    level = dict(name = 'baseline_kinematics_loose_JECvariation',
+                 All = (dict(factory = 'LambdaStrFactory', lambda_str = "ev : ev.nJet100JECUp[0] >= 1", name = 'nJet100_JECvar'),
+                        dict(factory = 'LambdaStrFactory', lambda_str = "ev : ev.ht40JECUp[0] >= 150", name = 'ht40_loose_JECvar'),
+                 )
+                )
+    return FactoryDispatcher(level = level, **kargs)
 
 ##__________________________________________________________________||
