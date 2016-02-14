4d086d7ac35e39492ed368ec9ebc1876d8855188
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-14 15:25:37 +0100

    move the dict lambdaStrDict to its own file

--- EventSelectionLevels/baseline_kinematics_loose_JECvariation.py
+++ EventSelectionLevels/baseline_kinematics_loose_JECvariation.py
@@ -2,13 +2,7 @@ from .FactoryDispatcher import FactoryDispatcher
 
 ##__________________________________________________________________||
 def baseline_kinematics_loose_JECvariation(**kargs):
-    lambdaStrDict = {
-        'nJet100_JECvar' : 'ev : ev.nJet100JECUp[0] >= 1',
-        'ht40_loose_JECvar' : 'ev : ev.ht40JECUp[0] >= 150',
-    }
-
     level = dict(name = 'baseline_kinematics_loose_JECvariation',
-                 lambdaStrDict = lambdaStrDict,
                  All = (dict(factory = 'LambdaStrFromDictFactory', key = 'nJet100_JECvar'),
                         dict(factory = 'LambdaStrFromDictFactory', key = 'ht40_loose_JECvar'),
                  )
