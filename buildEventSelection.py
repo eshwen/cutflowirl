3da8f8490c0c629bc3e382388ce4f6eb339cec3f
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-14 13:36:17 +0100

    use FactoryDispatcher in buildEventSelection

--- buildEventSelection.py
+++ buildEventSelection.py
@@ -1,7 +1,9 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
 from .EventSelectionLevels.Modules.EventSelectionAll import EventSelectionAll
 from .EventSelectionLevels.Modules.EventSelectionAny import EventSelectionAny
+from .EventSelectionLevels.Modules.EventSelectionNot import EventSelectionNot
 from .EventSelectionLevels.AllFactory import AllFactory
+from .EventSelectionLevels.FactoryDispatcher import FactoryDispatcher
 
 import os, sys
 
@@ -10,7 +12,15 @@ thisDir = os.path.dirname(os.path.realpath(__file__))
 if not thisDir in sys.path: sys.path.append(thisDir)
 
 ##__________________________________________________________________||
-def buildEventSelection(levels, name = 'All', AllClass = EventSelectionAll, AnyClass = EventSelectionAny, **kargs):
-    return AllFactory(levels = levels, name = name, AllClass = AllClass, AnyClass = AnyClass, **kargs)
+## def buildEventSelection(levels, name = 'All', AllClass = EventSelectionAll, AnyClass = EventSelectionAny, **kargs):
+##    return AllFactory(levels = levels, name = name, AllClass = AllClass, AnyClass = AnyClass, **kargs)
+
+##__________________________________________________________________||
+def buildEventSelection(**kargs):
+    return FactoryDispatcher(
+        AllClass = EventSelectionAll,
+        AnyClass = EventSelectionAny,
+        NotClass = EventSelectionNot,
+        **kargs)
 
 ##__________________________________________________________________||
