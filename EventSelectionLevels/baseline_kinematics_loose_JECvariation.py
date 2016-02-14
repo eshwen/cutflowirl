116d8e5a6371dffa0295cd72d22b78171c15aff8
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-14 15:18:34 +0100

    use LambdaStrFromDictFactory in baseline_kinematics_loose_JECvariation()

--- EventSelectionLevels/baseline_kinematics_loose_JECvariation.py
+++ EventSelectionLevels/baseline_kinematics_loose_JECvariation.py
@@ -2,9 +2,15 @@ from .FactoryDispatcher import FactoryDispatcher
 
 ##__________________________________________________________________||
 def baseline_kinematics_loose_JECvariation(**kargs):
+    lambdaStrDict = {
+        'nJet100_JECvar' : 'ev : ev.nJet100JECUp[0] >= 1',
+        'ht40_loose_JECvar' : 'ev : ev.ht40JECUp[0] >= 150',
+    }
+
     level = dict(name = 'baseline_kinematics_loose_JECvariation',
-                 All = (dict(factory = 'LambdaStrFactory', lambda_str = "ev : ev.nJet100JECUp[0] >= 1", name = 'nJet100_JECvar'),
-                        dict(factory = 'LambdaStrFactory', lambda_str = "ev : ev.ht40JECUp[0] >= 150", name = 'ht40_loose_JECvar'),
+                 lambdaStrDict = lambdaStrDict,
+                 All = (dict(factory = 'LambdaStrFromDictFactory', key = 'nJet100_JECvar'),
+                        dict(factory = 'LambdaStrFromDictFactory', key = 'ht40_loose_JECvar'),
                  )
                 )
     return FactoryDispatcher(level = level, **kargs)
