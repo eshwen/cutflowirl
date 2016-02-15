5b55f7fb5c0551d7f37293318ccf96c1ecfd5dbf
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-15 20:27:44 +0100

    move lambdaStrDict from htbin_alphaT to lambdaStrDict.py

--- EventSelectionLevels/htbin_alphaT.py
+++ EventSelectionLevels/htbin_alphaT.py
@@ -1,23 +1,7 @@
 from .FactoryDispatcher import FactoryDispatcher
-import copy
 
 ##__________________________________________________________________||
 def htbin_alphaT(AllClass, AnyClass, **kargs):
-
-    kargs_copy = copy.deepcopy(kargs)
-
-    lambdaStrDict = {
-        'htbin_200': "ev : ev.htbin[0] == 200",
-        'htbin_250': "ev : ev.htbin[0] == 250",
-        'htbin_300': "ev : ev.htbin[0] == 300",
-        'htbin_350': "ev : ev.htbin[0] == 350",
-        'htbin_400': "ev : ev.htbin[0] == 400",
-        'htbin_600': "ev : ev.htbin[0] == 600",
-        'htbin_800': "ev : ev.htbin[0] == 800",
-        'alphaT': "ev : {v} <= ev.alphaT[0]",
-    }
-
-    kargs_copy['lambdaStrDict'].update(lambdaStrDict)
 
     level = dict(
         Any = (dict(All = ('htbin_200', ('alphaT', dict(v = 0.65)))),
@@ -32,7 +16,7 @@ def htbin_alphaT(AllClass, AnyClass, **kargs):
 
     return FactoryDispatcher(
         AllClass = AllClass, AnyClass = AnyClass,
-        level = level, **kargs_copy)
+        level = level, **kargs)
 
 
 ##__________________________________________________________________||
