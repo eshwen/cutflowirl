1248ef97502459e060f18ef1cfbca2acc2761d02
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-16 07:48:06 +0100

    remove commented out lines in  buildEventSelection.py

--- buildEventSelection.py
+++ buildEventSelection.py
@@ -13,10 +13,6 @@ thisDir = os.path.dirname(os.path.realpath(__file__))
 if not thisDir in sys.path: sys.path.append(thisDir)
 
 ##__________________________________________________________________||
-## def buildEventSelection(levels, name = 'All', AllClass = EventSelectionAll, AnyClass = EventSelectionAny, **kargs):
-##    return AllFactory(levels = levels, name = name, AllClass = AllClass, AnyClass = AnyClass, **kargs)
-
-##__________________________________________________________________||
 def buildEventSelection(**kargs):
     return FactoryDispatcher(
         AllClass = EventSelectionAll,
