2a31ff2f2c19d99e64271b745ee44daf15849a67
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-16 17:25:10 +0100

    update LambdaStrFactory, support format

--- EventSelectionLevels/LambdaStrFactory.py
+++ EventSelectionLevels/LambdaStrFactory.py
@@ -1,7 +1,7 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
 
 ##__________________________________________________________________||
-def LambdaStrFactory(lambda_str, name = None, **kargs):
-    return kargs['LambdaStrClass'](lambda_str = lambda_str, name = name)
+def LambdaStrFactory(lambda_str, LambdaStrClass, name = None, **kargs):
+    return LambdaStrClass(lambda_str = lambda_str.format(**kargs), name = name)
 
 ##__________________________________________________________________||
