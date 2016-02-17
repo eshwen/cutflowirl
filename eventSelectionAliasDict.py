0249f4e032743edf6043a453d618c2da7a841fc3
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-17 20:35:18 +0100

    add isData, isMC to  eventSelectionAliasDict

--- eventSelectionAliasDict.py
+++ eventSelectionAliasDict.py
@@ -3,6 +3,8 @@
 eventSelectionAliasDict = {
     'True': "ev : True",
     'False': "ev : False",
+    'isData': "ev : ev.isData[0] == 1",
+    'isMC': "ev : ev.isData[0] == 0",
     'JSON': "ev : ev.inCertifiedLumiSections[0]",
     'ht40': "ev : ev.ht40[0] >= 200",
     'ht40_loose_JECvar': 'ev : ev.ht40JECUp[0] >= 150',
