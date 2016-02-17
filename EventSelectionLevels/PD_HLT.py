9b4892077a51f397fe9516a4491a158ea852f86d
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-17 09:18:04 +0100

    reimplement PD_HLT() with FactoryDispatcher()

--- EventSelectionLevels/PD_HLT.py
+++ EventSelectionLevels/PD_HLT.py
@@ -1,72 +1,51 @@
-from .Modules.LambdaStr import LambdaStr
+from .FactoryDispatcher import FactoryDispatcher
 
 ##__________________________________________________________________||
-def PD_HLT(AllClass, AnyClass, datamc, **kargs):
+def PD_HLT(**kargs):
 
-    if not datamc == 'data': return AllClass(name = 'PD_HLT')
+    path_cfg = dict(Any = (
+        dict(All = ('PD_MET', dict(Any = (
+            # 'HLT_PFMET90_PFMHT90_IDTight',
+            'HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight',
+            'HLT_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDTight',
+        )))),
+        dict(All = ('PD_HTMHT', dict(Any = (
+            'HLT_PFHT200_DiPFJetAve90_PFAlphaT0p57',
+            'HLT_PFHT200_DiPFJetAve90_PFAlphaT0p63',
+            'HLT_PFHT250_DiPFJetAve90_PFAlphaT0p55',
+            'HLT_PFHT250_DiPFJetAve90_PFAlphaT0p58',
+            'HLT_PFHT300_DiPFJetAve90_PFAlphaT0p53',
+            'HLT_PFHT300_DiPFJetAve90_PFAlphaT0p54',
+            'HLT_PFHT350_DiPFJetAve90_PFAlphaT0p52',
+            'HLT_PFHT350_DiPFJetAve90_PFAlphaT0p53',
+            'HLT_PFHT400_DiPFJetAve90_PFAlphaT0p51',
+            'HLT_PFHT400_DiPFJetAve90_PFAlphaT0p52',
+        )))),
+        dict(All = ('PD_JetHT', dict(Any = (
+            'HLT_PFHT800',
+        )))),
+        dict(All = ('PD_SingleMuon', dict(Any = (
+            'HLT_IsoMu17_eta2p1',
+            'HLT_IsoMu20',
+            'HLT_IsoTkMuMu20',
+            'HLT_IsoMu24_eta2p1',
+        )))),
+        dict(All = ('PD_SingleElectron', dict(Any = (
+            'HLT_Ele22_WPLoose_Gsf',
+            'HLT_Ele23_WPLoose_Gsf',
+            'HLT_Ele27_eta2p1_WPLoose_Gsf',
+        )))),
+        dict(All = ('PD_SinglePhoton', dict(Any = (
+            'HLT_Photon120',
+            'HLT_Photon125',
+            'HLT_Photon175',
+        )))),
+        )
+    )
 
-    ret = AnyClass(name = 'PD_HLT')
+    ## import pprint
+    ## pprint.pprint(path_cfg)
 
-    pd = AllClass(name = 'MET')
-    pd.add(LambdaStr("ev : ev.PrimaryDataset[0] == 'MET'", name = 'PDMET'))
-    hlt = AnyClass(name = 'MET_HLT')
-    ## hlt.add(LambdaStr("ev : ev.HLT_PFMET90_PFMHT90_IDTight[0]", name = 'HLT_PFMET90_PFMHT90_IDTight'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight[0]", name = 'HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDTight[0]", name = 'HLT_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDTight'))
-    pd.add(hlt)
-    ret.add(pd)
-
-    pd = AllClass(name = 'HTMHT')
-    pd.add(LambdaStr("ev : ev.PrimaryDataset[0] == 'HTMHT'", name = 'PDHTMHT'))
-    hlt = AnyClass(name = 'HTMHT_HLT')
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT200_DiPFJetAve90_PFAlphaT0p57[0]", name = 'HLT_PFHT200_DiPFJetAve90_PFAlphaT0p57'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT200_DiPFJetAve90_PFAlphaT0p63[0]", name = 'HLT_PFHT200_DiPFJetAve90_PFAlphaT0p63'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT250_DiPFJetAve90_PFAlphaT0p55[0]", name = 'HLT_PFHT250_DiPFJetAve90_PFAlphaT0p55'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT250_DiPFJetAve90_PFAlphaT0p58[0]", name = 'HLT_PFHT250_DiPFJetAve90_PFAlphaT0p58'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT300_DiPFJetAve90_PFAlphaT0p53[0]", name = 'HLT_PFHT300_DiPFJetAve90_PFAlphaT0p53'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT300_DiPFJetAve90_PFAlphaT0p54[0]", name = 'HLT_PFHT300_DiPFJetAve90_PFAlphaT0p54'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT350_DiPFJetAve90_PFAlphaT0p52[0]", name = 'HLT_PFHT350_DiPFJetAve90_PFAlphaT0p52'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT350_DiPFJetAve90_PFAlphaT0p53[0]", name = 'HLT_PFHT350_DiPFJetAve90_PFAlphaT0p53'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT400_DiPFJetAve90_PFAlphaT0p51[0]", name = 'HLT_PFHT400_DiPFJetAve90_PFAlphaT0p51'))
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT400_DiPFJetAve90_PFAlphaT0p52[0]", name = 'HLT_PFHT400_DiPFJetAve90_PFAlphaT0p52'))
-    pd.add(hlt)
-    ret.add(pd)
-
-    pd = AllClass(name = 'JetHT')
-    pd.add(LambdaStr("ev : ev.PrimaryDataset[0] == 'JetHT'", name = 'PDJetHT'))
-    hlt = AnyClass(name = 'JetHT_HLT')
-    hlt.add(LambdaStr("ev : ev.HLT_PFHT800[0]", name = 'HLT_PFHT800'))
-    pd.add(hlt)
-    ret.add(pd)
-
-    pd = AllClass(name = 'SingleMuon')
-    pd.add(LambdaStr("ev : ev.PrimaryDataset[0] == 'SingleMuon'", name = 'PDSingleMuon'))
-    hlt = AnyClass(name = 'SingleMuon_HLT')
-    hlt.add(LambdaStr("ev : ev.HLT_IsoMu17_eta2p1[0]", name = 'HLT_IsoMu17_eta2p1'))
-    hlt.add(LambdaStr("ev : ev.HLT_IsoMu20[0]",        name = 'HLT_IsoMu20'))
-    hlt.add(LambdaStr("ev : ev.HLT_IsoTkMuMu20[0]",    name = 'HLT_IsoTkMuMu20'))
-    hlt.add(LambdaStr("ev : ev.HLT_IsoMu24_eta2p1[0]", name = 'HLT_IsoMu24_eta2p1'))
-    pd.add(hlt)
-    ret.add(pd)
-
-    pd = AllClass(name = 'SingleElectron')
-    pd.add(LambdaStr("ev : ev.PrimaryDataset[0] == 'SingleElectron'", name = 'PDSingleElectron'))
-    hlt = AnyClass(name = 'SingleElectron_HLT')
-    hlt.add(LambdaStr("ev : ev.HLT_Ele22_WPLoose_Gsf[0]", name = 'HLT_Ele22_WPLoose_Gsf'))
-    hlt.add(LambdaStr("ev : ev.HLT_Ele23_WPLoose_Gsf[0]", name = 'HLT_Ele23_WPLoose_Gsf'))
-    hlt.add(LambdaStr("ev : ev.HLT_Ele27_eta2p1_WPLoose_Gsf[0]", name = 'HLT_Ele27_eta2p1_WPLoose_Gsf'))
-    pd.add(hlt)
-    ret.add(pd)
-
-    pd = AllClass(name = 'SinglePhoton')
-    pd.add(LambdaStr("ev : ev.PrimaryDataset[0] == 'SinglePhoton'", name = 'PDSinglePhoton'))
-    hlt = AnyClass(name = 'SinglePhoton_HLT')
-    hlt.add(LambdaStr("ev : ev.HLT_Photon120[0]", name = 'HLT_Photon120'))
-    hlt.add(LambdaStr("ev : ev.HLT_Photon125[0]", name = 'HLT_Photon125'))
-    hlt.add(LambdaStr("ev : ev.HLT_Photon175[0]", name = 'HLT_Photon175'))
-    pd.add(hlt)
-    ret.add(pd)
-
-    return ret
+    return FactoryDispatcher(path_cfg = path_cfg, **kargs)
 
 ##__________________________________________________________________||
