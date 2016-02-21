9512fe4bd792807720a4910a32f0a4e5e8d0dd78
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-21 06:40:00 +0100

    clean up the code in inEventList

--- Scribblers/inEventList.py
+++ Scribblers/inEventList.py
@@ -9,25 +9,18 @@ class inEventList(object):
         self.eventList = set()
         for path in self.event_list_paths:
             self.eventList.update({l.strip() for l in open(path)})
-            # self.eventList.update({tuple([int(w) for w in l.strip().split(':')]) for l in open(path)})
 
     def begin(self, event):
         self.vals = [ ]
         setattr(event, self.branch_name, self.vals)
 
-#         self.eventList = set()
-#         for path in self.event_list_paths:
-#             self.eventList.update({tuple([int(w) for w in l.strip().split(':')]) for l in open(path)})
-#             # set of tuples of (run, lumi, evt)
-#             # e.g., set([(260627, 1708, 3096828758), (260627, 1708, 3097158216)])
-
-
     def event(self, event):
         setattr(event, self.branch_name, self.vals)
-        # self.vals[:] = [(event.run[0], event.lumi[0], event.evt[0]) in self.eventList]
-        self.vals[:] = ["{}:{}:{}".format(event.run[0], event.lumi[0], event.evt[0]) in self.eventList]
-#        if self.vals[0]:
-#            print (event.run[0], event.lumi[0], event.evt[0]), self.vals[0]
+
+        evId = "{}:{}:{}".format(event.run[0], event.lumi[0], event.evt[0])
+        # e.g., 260627:1708:3096828758
+
+        self.vals[:] = [evId in self.eventList]
 
     def end(self):
         self.eventList = None
