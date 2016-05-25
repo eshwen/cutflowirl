b599c371148d9d87d5d137137bd982a41bee0073
Tai Sakuma <tai.sakuma@gmail.com>
2016-05-25 11:35:48 +0200

    rename jet40pTOverMhtCosDPhi jetDphiAttrs, add arguments to __init__()

--- Scribblers/jetDphiAttrs.py
+++ Scribblers/jetDphiAttrs.py
@@ -2,7 +2,12 @@
 import numpy as np
 
 ##__________________________________________________________________||
-class jet40pTOverMhtCosDPhi(object):
+class jetDphiAttrs(object):
+    def __init__(self, inJetPrefix, outJetPrefix, minJetPt = None):
+        self.inJetPrefix = inJetPrefix
+        self.outJetPrefix = outJetPrefix
+        self.minJetPt = minJetPt
+
     def begin(self, event):
         self.mht = [ ]
 
@@ -22,7 +27,6 @@ class jet40pTOverMhtCosDPhi(object):
         self.sinDphiOverF = [ ]
         self.sinCappedDphiOverF = [ ]
 
-        ## self.sinDphiOverFPlusCosDphi = [ ]
         self.sinCappedDphiOverFPlusCosCappedDphi = [ ]
 
         self.sinCappedDphiLogOneOverF = [ ]
@@ -30,33 +34,38 @@ class jet40pTOverMhtCosDPhi(object):
         self._attach_to_event(event)
 
     def _attach_to_event(self, event):
-        event.mht_jet40 = self.mht
+        setattr(event, 'mht_{}'.format(self.outJetPrefix), self.mht)
 
-        event.jet40_pt = self.pt
-        event.jet40_phi = self.phi
-        event.jet40_ptOverMht = self.ptOverMht
+        setattr(event, '{}_pt'.format(self.outJetPrefix), self.pt)
 
-        event.jet40_dphi = self.dphi
-        event.jet40_cappedDphi = self.cappedDphi
+        setattr(event, '{}_phi'.format(self.outJetPrefix), self.phi)
+        setattr(event, '{}_ptOverMht'.format(self.outJetPrefix), self.ptOverMht)
 
-        event.jet40_sinDphi = self.sinDphi
-        event.jet40_cosDphi = self.cosDphi
+        setattr(event, '{}_dphi'.format(self.outJetPrefix), self.dphi) 
+        setattr(event, '{}_cappedDphi'.format(self.outJetPrefix), self.cappedDphi) 
 
-        event.jet40_sinCappedDphi = self.sinCappedDphi
-        event.jet40_cosCappedDphi = self.cosCappedDphi
+        setattr(event, '{}_sinDphi'.format(self.outJetPrefix), self.sinDphi) 
+        setattr(event, '{}_cosDphi'.format(self.outJetPrefix), self.cosDphi) 
 
-        event.jet40_sinDphiOverF = self.sinDphiOverF
-        event.jet40_sinCappedDphiOverF = self.sinCappedDphiOverF
+        setattr(event, '{}_sinCappedDphi'.format(self.outJetPrefix), self.sinCappedDphi) 
+        setattr(event, '{}_cosCappedDphi'.format(self.outJetPrefix), self.cosCappedDphi) 
 
-        ## event.jet40_sinDphiOverFPlusCosDphi = self.sinDphiOverFPlusCosDphi
-        event.jet40_sinCappedDphiOverFPlusCosCappedDphi = self.sinCappedDphiOverFPlusCosCappedDphi
+        setattr(event, '{}_sinDphiOverF'.format(self.outJetPrefix), self.sinDphiOverF)
+        setattr(event, '{}_sinCappedDphiOverF'.format(self.outJetPrefix), self.sinCappedDphiOverF) 
 
-        event.jet40_sinCappedDphiLogOneOverF = self.sinCappedDphiLogOneOverF
+        setattr(event, '{}_sinCappedDphiOverFPlusCosCappedDphi'.format(self.outJetPrefix), self.sinCappedDphiOverFPlusCosCappedDphi) 
+
+        setattr(event, '{}_sinCappedDphiLogOneOverF'.format(self.outJetPrefix), self.sinCappedDphiLogOneOverF) 
 
     def event(self, event):
         self._attach_to_event(event)
 
-        idxs = [i for i in range(len(event.jet_pt)) if event.jet_pt[i] > 40]
+        event_jet_pt = getattr(event, '{}_pt'.format(self.inJetPrefix))
+
+        idxs = range(len(event_jet_pt))
+        if self.minJetPt is not None:
+            idxs = [i for i in idxs if event_jet_pt[i] >= self.minJetPt]
+
         if not idxs:
             self.pt[:] = [ ]
             self.phi[:] = [ ]
@@ -73,11 +82,13 @@ class jet40pTOverMhtCosDPhi(object):
             self.cosCappedDphi[:] = [ ]
             return
 
-        self.pt[:] = [event.jet_pt[i] for i in idxs]
+        self.pt[:] = [event_jet_pt[i] for i in idxs]
+
+        event_jet_phi = getattr(event, '{}_phi'.format(self.inJetPrefix))
         self.phi[:] = [event.jet_phi[i] for i in idxs]
 
         pt = np.array(self.pt)
-        phi = np.array(self.phi[:])
+        phi = np.array(self.phi)
 
         px = pt*np.cos(phi)
         py = pt*np.sin(phi)
