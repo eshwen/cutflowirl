9da03eb0981daa859335d5c4556277f05b91c929
Tai Sakuma <tai.sakuma@gmail.com>
2016-05-29 16:12:43 +0200

    update xi

--- Scribblers/xi.py
+++ Scribblers/xi.py
@@ -4,22 +4,22 @@ import numpy as np
 ##__________________________________________________________________||
 class xi(object):
     def begin(self, event):
-        self.tanXi = [ ]
         self.xi = [ ]
         self._attach_to_event(event)
 
     def _attach_to_event(self, event):
-        event.tanXi = self.tanXi
         event.xi = self.xi
 
     def event(self, event):
         self._attach_to_event(event)
-        if len(event.jet40_pt) <= 1:
-            self.tanXi[:] = [-1]
+
+        sinMinDphiTilde = np.sin(event.minDphiTilde)
+        maxH = np.array(event.maxH)
+
+        if not (sinMinDphiTilde.size == 1 and maxH.size == 1):
             self.xi[:] = [-1]
             return
 
-        self.tanXi[:] = [event.minSinDphiTilde[0]/event.maxH[0]]
-        self.xi[:] = [np.arctan2(event.minSinDphiTilde[0], event.maxH[0])]
+        self.xi[:] = np.arctan2(sinMinDphiTilde, maxH)
 
 ##__________________________________________________________________||
