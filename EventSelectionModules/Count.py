88a5c5784aa0deb7eacf8560114cd279a99d471b
Tai Sakuma <tai.sakuma@gmail.com>
2016-08-28 10:26:56 +0200

    add operators to Count

--- EventSelectionModules/Count.py
+++ EventSelectionModules/Count.py
@@ -12,4 +12,24 @@ class Count(object):
             r[2] += 1 # total
             if p: r[1] += 1 # pass
 
+    def __add__(self, other):
+        ret = Count()
+        results = list(self._results) # copy
+        if not other == 0: # other is 0 when e.g. sum([obj1, obj2])
+            self._add_results_inplace(results, other._results)
+        ret._results[:] = results
+        return ret
+
+    def __iadd__(self, other):
+        self._add_results_inplace(self._results, other._results)
+        return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def _add_results_inplace(self, res1, res2):
+        for r1, r2 in zip (res1, res2):
+            r1[1] += r2[1]
+            r1[2] += r2[2]
+
 ##__________________________________________________________________||
--- tests/EventSelectionModules/test_Count.py
+++ tests/EventSelectionModules/test_Count.py
@@ -70,3 +70,48 @@ class Test_Count(unittest.TestCase):
             obj._results
         )
 ##__________________________________________________________________||
+class TestCount_operator(unittest.TestCase):
+
+    def setUp(self):
+        self.obj1 = Count()
+        self.obj2 = Count()
+
+        self.obj1._results  = [
+            [0, 2, 2],
+            [1, 1, 2],
+            [2, 0, 1],
+            ]
+
+        self.obj2._results  = [
+            [0, 3, 5],
+            [1, 2, 4],
+            [2, 1, 2],
+        ]
+
+        self.expected = [
+            [0, 5, 7],
+            [1, 3, 6],
+            [2, 1, 3],
+            ]
+
+    def test_add(self):
+        obj3 = self.obj1 + self.obj2
+        self.assertEqual(self.expected, obj3._results)
+        self.assertIsNot(self.obj1._results, obj3._results)
+        self.assertIsNot(self.obj2._results, obj3._results)
+
+    def test_radd(self):
+        obj3 = sum([self.obj1, self.obj2]) # 0 + obj1 is executed
+        self.assertEqual(self.expected, obj3._results)
+        self.assertIsNot(self.obj1._results, obj3._results)
+        self.assertIsNot(self.obj2._results, obj3._results)
+
+    def test_iadd(self):
+        obj1 = self.obj1
+
+        self.obj1 += self.obj2
+        self.assertIs(self.obj1, obj1)
+        self.assertEqual(self.expected, self.obj1._results)
+
+
+##__________________________________________________________________||
