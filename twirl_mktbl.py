444732679a2f7d684f0dbf4e5d89080ca1038532
Tai Sakuma <tai.sakuma@gmail.com>
2017-02-05 14:42:58 +0100

    copy files from the repo bdphi-scripts

---
+++ twirl_mktbl.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# Tai Sakuma <sakuma@fnal.gov>
+import os, sys
+import argparse
+import logging
+
+import AlphaTwirl
+
+##__________________________________________________________________||
+sys.path.insert(1, os.path.dirname(os.path.dirname(__file__)))
+
+##__________________________________________________________________||
+utilspy_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'utilspy')
+sys.path.insert(1, utilspy_path)
+import FrameworkHeppy
+
+##__________________________________________________________________||
+default_heppydir = '/Users/sakuma/work/cms/c150130_RA1_data/80X/MC/20161021_B03/ROC_MC_SM'
+
+##__________________________________________________________________||
+parser = argparse.ArgumentParser()
+parser.add_argument("--mc", action = "store_const", dest = 'datamc', const = 'mc', default = 'mc', help = "for processing MC")
+parser.add_argument("--data", action = "store_const", dest = 'datamc', const = 'data', help = "for processing data")
+parser.add_argument('--parallel-mode', default = 'multiprocessing', choices = ['multiprocessing', 'subprocess', 'htcondor'], help = "mode for concurrency")
+parser.add_argument('--profile', action = "store_true", help = "run profile")
+parser.add_argument('--profile-out-path', default = None, help = "path to write the result of profile")
+parser.add_argument('--add-susy-masspoints', action = 'store_true', default = False, help = 'add tables for SUSY mass points')
+
+parser.add_argument('-o', '--outDir', default = os.path.join('tbl', 'out'))
+parser.add_argument('-n', '--nevents', default = -1, type = int, help = 'maximum number of events to process for each component')
+parser.add_argument('--max-events-per-process', default = -1, type = int, help = 'maximum number of events per process')
+parser.add_argument('--force', action = 'store_true', default = False, dest='force', help = 'recreate all output files')
+
+parser.add_argument('--logging-level', default = 'WARN', choices = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'], help = "level for logging")
+
+parser.add_argument('-i', '--heppydir', default = default_heppydir, help = 'Heppy results dir')
+parser.add_argument('-p', '--processes', default = None, type = int, help = 'number of processes to run in parallel')
+parser.add_argument('-q', '--quiet', default = False, action = 'store_true', help = 'quiet mode')
+parser.add_argument('-c', '--components', default = None, nargs = '*', help = 'the list of components')
+
+args = parser.parse_args()
+
+##__________________________________________________________________||
+def main():
+
+    #
+    # configure logger
+    #
+    log_level = logging.getLevelName(args.logging_level)
+    log_handler = logging.StreamHandler()
+    log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    log_handler.setFormatter(log_formatter)
+
+    names_for_logger = ["FrameworkHeppy", "AlphaTwirl"]
+    for n in names_for_logger:
+        logger = logging.getLogger(n)
+        logger.setLevel(log_level)
+        logger.handlers[:] = [ ]
+        logger.addHandler(log_handler)
+
+    #
+    #
+    #
+    reader_collector_pairs = [ ]
+
+    #
+    # configure scribblers
+    #
+    scribblers = [ ]
+
+    reader_collector_pairs.extend([(r, AlphaTwirl.Loop.NullCollector()) for r in scribblers])
+
+    #
+    # configure event selections
+    #
+    path_cfg = dict(All = (
+        'ev : ev.nJet40[0] >= 3',
+        'ht40',
+        'nJet100',
+        dict(Any = ('ht40', 'nJet100')),
+        dict(Not = 'ht40'),
+    ))
+
+    ## import pprint
+    ## pprint.pprint(path_cfg)
+    from atlogic.EventSelectionModules.EventSelectionAllCount import EventSelectionAllCount
+    from atlogic.EventSelectionModules.EventSelectionAnyCount import EventSelectionAnyCount
+    from atlogic.EventSelectionModules.EventSelectionNotCount import EventSelectionNotCount
+
+    from atlogic.buildEventSelection import buildEventSelection
+    eventSelection = buildEventSelection(
+        path_cfg = path_cfg,
+        AllClass = EventSelectionAllCount,
+        AnyClass = EventSelectionAnyCount,
+        NotClass = EventSelectionNotCount
+    )
+
+    from atlogic.event_selection_str import event_selection_str
+    eventselection_path = os.path.join(args.outDir, 'eventselection.txt')
+    if args.force or not os.path.exists(eventselection_path):
+        AlphaTwirl.mkdir_p(os.path.dirname(eventselection_path))
+        f = open(eventselection_path, 'w')
+        f.write(event_selection_str(eventSelection))
+        f.close()
+
+    tbl_cutflow_path = os.path.join(args.outDir, 'tbl_cutflow.txt')
+
+    resultsCombinationMethod = AlphaTwirl.Collector.CombineIntoList(
+        summaryColumnNames = ('depth', 'class', 'name', 'pass', 'total'),
+        sort = False,
+        summarizer_to_tuple_list = summarizer_to_tuple_list
+    )
+    deliveryMethod = AlphaTwirl.Collector.WriteListToFile(tbl_cutflow_path)
+    collector = AlphaTwirl.Loop.Collector(resultsCombinationMethod, deliveryMethod)
+    reader_collector_pairs.append((eventSelection, collector))
+
+    #
+    # run
+    #
+    fw =  FrameworkHeppy.FrameworkHeppy(
+        outdir = args.outDir,
+        heppydir = args.heppydir,
+        datamc = args.datamc,
+        force = args.force,
+        quiet = args.quiet,
+        parallel_mode = args.parallel_mode,
+        process = args.processes,
+        user_modules = ('atlogic', 'table_configs'),
+        max_events_per_dataset = args.nevents,
+        max_events_per_process = args.max_events_per_process,
+        profile = args.profile,
+        profile_out_path = args.profile_out_path
+
+    )
+    fw.run(
+        components = args.components,
+        reader_collector_pairs = reader_collector_pairs
+    )
+
+##__________________________________________________________________||
+def summarizer_to_tuple_list(summarizer, sort):
+    return [tuple(e) for e in summarizer._results]
+
+##__________________________________________________________________||
+if __name__ == '__main__':
+    main()
